ca8fafe00ccae2c3b76d7630eec6f0d80c915552
renatajarmova <renatajarmova@gmail.com>
2020-01-15 15:42:08 +0000

    Add initial timer component to be refactored

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,8 +1,18 @@
-import React from "react";
+// import React from "react";
+// import { create } from "domain";
 
-const Timer = () => {
+// const Timer = props => {
+//     const [time, setTime] = React.useState(new Date());
+//     React.useEffect(() => { 
+//         const timerID = setTime
 
-}
+//         return (
+//             <div>
+//                 <p>{time.toLocaleTimeString}</p>
+//             </div>
+//         );
+//     })
+    
+// }
 
-export default Timer;
-
+// export default Timer;
