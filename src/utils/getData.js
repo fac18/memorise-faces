ddc4916135e7b2707ea48ce8e08a0aa41aa7b0c7
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-01-16 11:22:26 +0000

    Gave the form input to the api and fixed the api call to list all members of a team closes #20 #21

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,21 +1,25 @@
 const token = process.env.REACT_APP_TOKEN;
 
 const checkResponse = response => {
-    if (response.status !== 200) {
-        console.log(`Error with the request! ${response.status} `);
-        return;
-    }
-    return response.json();
+  if (response.status !== 200) {
+    console.log(`Error with the request! ${response.status} `);
+    return;
+  }
+  return response.json();
 };
 
-export const getTeamData = props => {
-    const org = "FAC18";
-    const team = "students";
+export const getTeamData = (player, org, team) => {
+  // const org = "FAC18";
+  // const team = "mentors";
+  // console.log({ props });
+  // const { org, team } = props;
 
-    return fetch(`https://api.github.com/orgs/${org}/teams/${team}?access_token=${token}`)
-        .then(checkResponse)
-        .then(data => console.log(data))
-        .catch(err => {
-            throw new Error(`fetch getTeamData failed ${err}`);
-        });
+  return fetch(
+    `https://api.github.com/orgs/${org}/teams/${team}/members?access_token=${token}`
+  )
+    .then(checkResponse)
+    .then(data => console.log(data))
+    .catch(err => {
+      throw new Error(`fetch getTeamData failed ${err}`);
+    });
 };
