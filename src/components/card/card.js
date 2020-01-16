5f2d4aad97a762e231b065bb685a8cb96a75c3e0
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-01-16 15:15:38 +0000

    Added randomising logic for cards and made sure the array does not shuffle on every re-render
    https://reactjs.org/docs/hooks-reference.html\#usememo
    closes #24

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,23 +1,55 @@
 import React from "react";
 
 const Card = props => {
-    // console.log(props.userData[1].login);
-    if(!props.userData) {
-        return <h2>Loading...</h2>;
-    }
-    const userArray = props.userData;
-    //const { login, avatar_url} = userArray;
-    const userList = userArray.map( user => (
+  const [view, setView] = React.useState(false);
+
+  const userArray = props.userData;
+  // a card that is blank
+  // toggle display with boolean
+  // randomise the way that we display the cards (names and photos)
+  const shuffle = arr => {
+    //spread operator here makes sure that the original array is not mutated and we are given a new array only
+    return [...arr].sort(() => 0.5 - Math.random());
+  };
+
+  let newArr = React.useMemo(() => {
+    //we've kept the arrays inside of useMemo because they are always changing
+    // (therefore they would be shuffled at every re-render)
+    //create array of images components
+    const imgArr = userArray.map(user => {
+      return (
         <li>
-            <h1>{user.login}</h1>
-            <img src={user.avatar_url} alt="User's avatar"></img>
+          <img
+            src={user.avatar_url}
+            className={user.login}
+            alt="User's avatar"
+          ></img>
         </li>
-    ));
-        return (
-            <section>
-                <ul>{userList}</ul>
-            </section>
-        );
+      );
+    });
+
+    // create array of name components
+    const nameArr = userArray.map(user => {
+      return (
+        <li>
+          <h1 className={user.login}>{user.login}</h1>
+        </li>
+      );
+    });
+
+    //join arrays and shuffle them
+    return shuffle(nameArr.concat(imgArr));
+  }, [userArray]); // array will only reshuffle when userArray (a prop) is changed - props should not change
+
+  if (!props.userData) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <section>
+      <ul>{newArr}</ul>
+      <button onClick={() => setView(!view)}>clickme</button>
+    </section>
+  );
 };
-    
+
 export default Card;
